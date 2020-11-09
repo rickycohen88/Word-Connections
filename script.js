7fbc51eb78f0d671cb852b1875c3eb8d52001a4f
RickyCohen <ricky.cohen88@gmail.com>
2020-11-09 13:18:46 -0600

    needed to clean up oginal merge from today

--- script.js
+++ script.js
@@ -26,11 +26,13 @@ $("#search-button").on("click", function () {
     console.log("search button clicked");
     googleApi();
     console.log("called google api");
-    
-});
-searchBtn.on("click", function () {
+})
+
+
+searchBtn.on("click", function(){
     SetHistory();
-});
+})
+
 
 function SetHistory() {
     let textItem = searchText.val();
@@ -41,11 +43,11 @@ function SetHistory() {
     newListItem.on("click", function(){historyClick()});
     $("#history-items").prepend(newListItem);
     idNumber++;
-};
+}
 
 function historyClick() {
     console.log("hi");
-};
+}
 
 
   // google api stuffssssss
@@ -69,10 +71,11 @@ function googleApi(){
         googleObject=responce;
         console.log(googleObject);
         createVideoCarusel();
-        }
-    )
-        
+        })
+    
 }
+
+
 
 
  function createVideoCarusel(){
@@ -81,14 +84,7 @@ function googleApi(){
   let video2 = "https://www.youtube.com/embed/"+video1;
   console.log(video2);
   document.getElementById("ytPlayer").setAttribute("src",video2);
+ };
  
    // ect
 
- }
-
-
-//!!!!!!!!!!!       this is the basics for embeded only vids    !!!!!!!!!!!!!!!!!!!!!!
- /*'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&order=relevance&q=banana&type=video&videoEmbeddable=true&videoType=videoTypeUnspecified&key=[YOUR_API_KEY]' \
-  --header 'Accept: application/json' \
-  --compressed
- */
