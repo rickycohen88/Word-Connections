8279a2dbcc820079a3b61ba95a3548df5e99ce9b
Kyle Galer <chembot81@hotmail.com>
2020-11-10 20:50:51 -0600

    dictionary api and definitions working

--- script.js
+++ script.js
@@ -13,27 +13,29 @@ let idNumber = 0;
 //let dict = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/"+searchText+"?key=097e4f17-51a3-4c33-868e-e4192b97f92a";
 //let thes = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/"+searchText+"?key=20fc6f58-5554-4511-aeb4-73b02f754ec4";
 
-
-searchBtn.on("click", function(){
+searchBtn.on("click", function () {
   let textItem = searchValue.val();
   searchValue.empty();
   SetHistory(textItem);
   googleApi(textItem);
-})
-
+  dictionaryAPI(textItem);
+});
 
 function SetHistory(textItem) {
   let newListItem = $("<li>");
   newListItem.attr("id", idNumber);
   newListItem.attr("class", "historyListItems");
   newListItem.append(textItem);
-  newListItem.on("click", function(){getHistory(textItem)});
+  newListItem.on("click", function () {
+    getHistory(textItem);
+  });
   $("#history-items").prepend(newListItem);
   idNumber++;
 }
 
 function getHistory(textItem) {
   googleApi(textItem);
+  dictionaryAPI(textItem);
 }
 
 // google api stuffssssss
@@ -44,26 +46,48 @@ function getHistory(textItem) {
 --compressed
 */
 
-function googleApi(searchText){
-  
+function googleApi(searchText) {
   let apiKey = "&key=AIzaSyAa4_ZX-UHSjDpcWGY4M_rfq0jS3mbIrbI";
-  let youtubeAPI = "https://youtube.googleapis.com/youtube/v3/search?&maxResults=10&order=relevance&q="+ searchText+ "&type=video&videoEmbeddable=true&videoType=videoTypeUnspecified" + apiKey;
+  let youtubeAPI =
+    "https://youtube.googleapis.com/youtube/v3/search?&maxResults=10&order=relevance&q=" +
+    searchText +
+    "&type=video&videoEmbeddable=true&videoType=videoTypeUnspecified" +
+    apiKey;
   console.log(youtubeAPI);
 
-    $.ajax({url:youtubeAPI,method: "GET"})
-    .then(function(responce){
-        console.log(responce);
-        googleObject=responce;
-        console.log(googleObject);
-        createVideoCarusel();
-      })
-    
+  $.ajax({ url: youtubeAPI, method: "GET" }).then(function (responce) {
+    console.log(responce);
+    googleObject = responce;
+    console.log(googleObject);
+    createVideoCarusel();
+  });
 }
 
-function createVideoCarusel(){
+function createVideoCarusel() {
   let video1 = googleObject.items[0].id.videoId;
   console.log(video1);
-  let video2 = "https://www.youtube.com/embed/"+video1;
+  let video2 = "https://www.youtube.com/embed/" + video1;
   console.log(video2);
-  document.getElementById("ytPlayer").setAttribute("src",video2);
- };
+  document.getElementById("ytPlayer").setAttribute("src", video2);
+}
+
+function dictionaryAPI(searchText) {
+$("#definitions").empty();
+let dict =
+  "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" +
+  searchText +
+  "?key=097e4f17-51a3-4c33-868e-e4192b97f92a";
+$.ajax({
+  url: dict,
+  method: "GET",
+}).then(function (response) {
+  $("#searched-word").text(searchText);
+  for (let i = 0; i < response[0].shortdef.length; i++) {
+    let listItem = $("<li>");
+    listItem.text(response[0].shortdef[i]);
+    $("#definitions").append(listItem);
+  }
+  $("#definition").empty();
+  console.log(response);
+});
+}
